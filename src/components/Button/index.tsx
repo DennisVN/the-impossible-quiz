7d26ccad1b1f4f7b6839626922717c527ebb8ba3
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-16 14:36:19 +0200

    Create JSON object, fetch data in console ( Quizcontainer), WIP fetch question and display on screen in SC

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import {Button} from 'react-bootstrap';
 
 const StyledButton = styled(Button)`
  width:auto; `;
-// ...props altijd als laatste benoemen
+
 const CustomButton: FC <any> = ({label, variant, onClick}) => {
     return  <StyledButton onClick={onClick} variant={variant} label={label}>{label}</StyledButton>;
 }
--- src/container/QuizContainer/index.tsx
+++ src/container/QuizContainer/index.tsx
@@ -1,18 +1,40 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { InputGroup } from "react-bootstrap";
-import match ,{ Redirect, Route, Switch } from "react-router-dom";
 import CustomButton from "../../components/Button";
 import InputField from "../../components/InputField";
 import TestStyledComponent from "../../components/TestStyledComponent";
 // Eindcomponenten komen hier terecht. 
 // TSC is main container aka form
+
+const getData=()=>{
+    fetch('Questions.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+    .then(function(response) {
+        //console.log(response)
+        return response.json();
+    })
+    .then(function(Questions) {
+        console.log(Questions);
+    });
+}
+
 const handleClick = () => {
     console.log('clickedy');
 }
 const QuizContainer: FC <any> = () => {
+    useEffect (() => {
+        getData()
+    }, [])
+
     return  <TestStyledComponent>
                 <div>
-                <InputGroup className="m-2">
+                <InputGroup>
                     <InputField placeholder="Enter your guess here" label="userInput" />
                     <CustomButton label="Submit" variant="secondary" onClick={handleClick}/>
                 </InputGroup>
